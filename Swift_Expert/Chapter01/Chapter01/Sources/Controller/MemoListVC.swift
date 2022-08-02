b71be5383edb3a7435ceadc4613638036c5da149
kangddong <rkdehddud96@naver.com>
2022-08-03 01:42:33 +0900

    Set UI and Make CustomClass

---
+++ Swift_Expert/Chapter01/Chapter01/Sources/Controller/MemoListVC.swift
@@ -0,0 +1,19 @@
+//
+//  MemoListVC.swift
+//  Chapter01
+//
+//  Created by 강동영 on 2022/08/02.
+//
+
+import UIKit
+
+class MemoListVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
