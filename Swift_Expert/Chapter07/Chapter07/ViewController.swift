bcdabb465537c0463820d7f7cd28e2b14f358b17
kangddong <rkdehddud96@naver.com>
2022-10-06 23:54:42 +0900

    CRUD Done

--- Swift_Expert/Chapter07/Chapter07/ViewController.swift
+++ Swift_Expert/Chapter07/Chapter07/ViewController.swift
@@ -1,19 +1,170 @@
 //
-//  ViewController.swift
+//  ListVC.swift
 //  Chapter07
 //
 //  Created by 강동영 on 2022/10/06.
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ListVC: UITableViewController {
 
+    lazy var list: [NSManagedObject] = {
+        return self.fetch()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let addBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add(_:)))
+        self.navigationItem.rightBarButtonItem = addBtn
+    }
+    
+    @objc func add(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "게시글 등록", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField { $0.placeholder = "제목" }
+        alert.addTextField { $0.placeholder = "내용" }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Save", style: .default) { (_) in
+            guard let title = alert.textFields?.first?.text,
+                  let  contents = alert.textFields?.last?.text
+            else { return }
+            
+            if self.save(title: title, contents: contents) == true {
+                self.tableView.reloadData()
+            }
+        })
+        self.present(alert, animated: false)
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let record = self.list[indexPath.row]
+        let title = record.value(forKey: "title") as? String
+        let contents = record.value(forKey: "contents") as? String
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = title
+        cell.detailTextLabel?.text = contents
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        
+        return .delete
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let object = self.list[indexPath.row]
+        
+        if self.delete(object: object) {
+            
+            self.list.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let object = self.list[indexPath.row]
+        let title = object.value(forKey: "title") as? String
+        let contents = object.value(forKey: "contents") as? String
+        let alert = UIAlertController(title: "게시글 수정", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField { $0.text = title }
+        alert.addTextField { $0.text = contents }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Save", style: .default) { (_) in
+            guard let title = alert.textFields?.first?.text,
+                  let  contents = alert.textFields?.last?.text
+            else { return }
+            
+            if self.edit(object: object, title: title, contents: contents) == true {
+                self.tableView.reloadData()
+            }
+        })
+        self.present(alert, animated: false)
+    }
+}
 
+extension ListVC {
+    
+    func fetch() -> [NSManagedObject] {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Board")
+        let result = try! context.fetch(fetchRequest)
+        return result
+    }
+    
+    func save(title: String, contents: String) -> Bool {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let object = NSEntityDescription.insertNewObject(forEntityName: "Board",
+                                                         into: context)
+        
+        object.setValue(title, forKey: "title")
+        object.setValue(contents, forKey: "contents")
+        object.setValue(Date(), forKey: "regdate")
+        
+        do {
+            try context.save()
+            self.list.append(object)
+            return true
+        } catch {
+            context.rollback()
+            return false
+        }
+    }
+    
+    func edit(object: NSManagedObject, title: String, contents: String) -> Bool {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let object = NSEntityDescription.insertNewObject(forEntityName: "Board",
+                                                         into: context)
+        
+        object.setValue(title, forKey: "title")
+        object.setValue(contents, forKey: "contents")
+        object.setValue(Date(), forKey: "regdate")
+        
+        do {
+            try context.save()
+            return true
+        } catch {
+            context.rollback()
+            return false
+        }
+    }
+    
+    func delete(object: NSManagedObject) -> Bool {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        context.delete(object)
+        
+        do {
+            try context.save()
+            self.list.append(object)
+            return true
+        } catch {
+            context.rollback()
+            return false
+        }
+    }
 }
 
